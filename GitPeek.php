58b880024ca6cfb5d1737baddde949e014a28888
Benno <Benno@bkpages.de>
2025-07-17 19:30:57 +0200

    GitPeek.php: duplicate button removed

--- GitPeek.php
+++ GitPeek.php
@@ -274,7 +274,6 @@ if (($level == 2 || $level == 3) && !repoExists($repoRoot, $repo)) {
   <div class="hl-right">
 		<button class="theme-switcher" id="themeBtn" title="Switch theme"><?=htmlspecialchars($theme)?> &#x25BC;</button>
 </br>
-		<button class="theme-switcher" id="fontBtn" title="Switch theme">Fonts</button>
 	</form>
     <div class="theme-popup" id="themePopup" role="menu">
       <?php foreach ($themes as $t => $css): ?>
