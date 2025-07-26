00c3429beef7f5f2f4b85fe4d5bd776e9e06182d
Benno <Benno@bkpages.de>
2025-07-26 13:40:04 +0200

    fontlink.php: just as sample and placeholder - not included anywhere

---
+++ fontlink.php
@@ -0,0 +1,4 @@
+
+<?php>
+<link rel="stylesheet" href="https://fonts.bunny.net/css?family='system-ui:400|Open+Sans:400|Roboto:400|ABeeZee:400|Abyssinica+SIL:400|Acme:400|Actor:400|Aldrich:400|Annie+Use+Your+Telescope:400|Damion:400|M+PLUS+1+Code:400'">
+?>
